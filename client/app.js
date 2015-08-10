899d0d05e4d1c4f3d924e72a6ba16b5b5d57f83c
Paul Sokolik <paul.sokolik@gmail.com>
2015-08-10 11:27:01 -0700

    complete simple/naive range end-point

---
+++ client/app.js
@@ -0,0 +1,8 @@
+$.ajax({
+  url: 'http://interview.carlypso.com/sort',
+  // type: 'GET',
+  success: function(data) {
+    console.log(data);
+  }
+});
+
--- server/server.js
+++ server/server.js
@@ -3,20 +3,76 @@ var request = require('request');
 var app = express();
 var port = 8080;
 
+app.use(express.static(__dirname + "/../client"));
+
 var carData;
-request('http://interview.carlypso.com/listings?offset=0&limit=10000', function (err, res, body) {
+request('http://interview.carlypso.com/listings?offset=0&limit=10', function (err, res, body) {
   if(err) {
     console.log('Error requesting data');
   } else {
-    console.log(body);
-    carData = body;
+    var data = JSON.parse(body);
+    carData = data.value;
+    console.log(carData);
   }
 });
 
-app.get('/signup', function(req, res) {
-  res.render('signup');
+var merge = function(left, right, param) {
+  var result  = [];
+  var leftIndex = 0;
+  var rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length){
+    console.log(left[leftIndex][param], right[rightIndex][param]);
+    if (left[leftIndex][param] < right[rightIndex][param]){
+        result.push(left[leftIndex++]);
+    } else {
+        result.push(right[rightIndex++]);
+    }
+  }
+  console.log(result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex)));
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+};
+
+var mergeSort = function(collection, param) {
+  if(collection.length < 2) {
+      return collection;
+  }
+  var middle = Math.floor(collection.length / 2);
+  var left    = collection.slice(0, middle);
+  var right   = collection.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right), param);
+};
+
+app.get('/sort', function(req, res) {
+  console.log('requesting', req.query);
+  // res.send(req.query);
+  var sorted;
+  var sort = req.query;
+  for(var key in sort) {
+    console.log('sorting on', key);
+    sorted = mergeSort(carData, key);
+  }
+  res.send(sorted);
 });
 
+app.get('/range', function(req, res) {
+  var range = req.query.range;
+  ranges = range.split('-');
+  ranges[0] = parseInt(ranges[0]);
+  ranges[1] = parseInt(ranges[1]);
+  var results = [];
+
+  for(var i=0; i<carData.length; i++) {
+    console.log(carData[i].price);
+    console.log(ranges[0], ranges[1]);
+    if(carData[i].price >= ranges[0] && carData[i].price <= ranges[1]) {
+      results.push(carData[i]);
+    }
+  }
+
+  res.send(results);
+});
 
 app.listen(port);
 console.log('Server now listening on port ' + port);
